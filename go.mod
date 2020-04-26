6072709df40f44277884215e639b32eafbe4b49b
Benji Vesterby <benji@devnw.com>
2020-04-26 16:53:31 -0500

    changes to get the system working again

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/devnw/atomizer-agent
 go 1.14
 
 require (
-	github.com/devnw/alog v1.0.1
-	github.com/devnw/atomizer v0.0.0-20200425014640-bf9d01a48c35
-	github.com/devnw/conductors v0.0.0-20200425195205-f8a2c989296a
-	github.com/devnw/montecarlopi v0.0.0-20200425201014-bca13b8f88d2
+	github.com/devnw/alog v1.0.2-0.20200426214902-d7dd58b863d0
+	github.com/devnw/amqp v0.0.0-20200426215044-99868c01ae25
+	github.com/devnw/atomizer v0.0.0-20200426214931-63adb38aac5b
+	github.com/devnw/montecarlopi v0.0.0-20200426215143-0391bf83b13a
 	github.com/pkg/errors v0.9.1
 )
--- main.go
+++ main.go
@@ -3,31 +3,32 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
 	"github.com/devnw/alog"
+	"github.com/devnw/amqp"
 	"github.com/devnw/atomizer"
-	amqp "github.com/devnw/conductors/amqp"
 	_ "github.com/devnw/montecarlopi"
 	"github.com/pkg/errors"
 )
 
 const (
-	// CONNECTIONSTRING is the connection string for the message queue, in this case
-	// this is specific to rabbit mq
+	// CONNECTIONSTRING is the connection string for the message queue,
+	// in this case this is specific to rabbitmq
 	CONNECTIONSTRING string = "CONNECTIONSTRING"
 
-	// QUEUE is the queue for atom messages to be passed across in the message queue
+	// QUEUE is the queue for atom messages to be passed across in the
+	// message queue
 	QUEUE string = "QUEUE"
 )
 
 func main() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
 	ctx, cancel := context.WithCancel(context.Background())
 
 	// Setup interrupt monitoring for the agent
@@ -38,91 +39,86 @@ func main() {
 		case <-ctx.Done():
 			return
 		case <-sigs:
-			alog.Println("Interrupt Received, Closing Atomizer Agent")
+			alog.Println("Closing Atomizer Agent")
 		}
 	}()
 
-	var err error
-
-	//[]alog.Destination{
-	// 	{
-	// 		alog.ERROR | alog.CRIT | alog.FATAL,
-	// 		alog.STD,
-	// 		os.Stderr,
-	// 	},
-	// }
-
-	if err = alog.Global(
+	err := alog.Global(
 		ctx,
 		"ATOMIZER AGENT",
 		alog.DEFAULTTIMEFORMAT,
 		time.UTC,
 		alog.DEFAULTBUFFER,
 		alog.Standards()...,
-	); err == nil {
-		env := flag.Bool("e", false, "signals to the agent to use environment variables for configurations")
-		c := flag.String("conn", "amqp://guest:guest@localhost:5672/", "connection string used for rabbit mq")
-		q := flag.String("queue", "atomizer", "queue is the queue for atom messages to be passed across in the message queue")
-		flag.Parse()
+	)
 
-		if *env {
-			*c, *q, err = envoverride()
-		}
-
-		if err == nil {
-
-			// Create a copy of the conductor for the agent
-			var conductor atomizer.Conductor
-			if conductor, err = amqp.Connect(ctx, *c, *q); err == nil {
-
-				// Register the conductor into the atomizer library after initializing the
-				/// connection to the message queue
-				err := atomizer.Register(conductor)
-				if err != nil {
-					panic(err)
-				}
-
-				if conductor != nil {
-					events := make(chan interface{})
-					alog.Printc(ctx, events)
-					// Create a copy of the atomizer
-					if mizer := atomizer.Atomize(ctx, events); mizer != nil {
-
-						// Execute the processing on the atomizer
-						if err = mizer.Exec(); err == nil {
-
-							alog.Println("Online")
-
-							// Block until the processing is interrupted
-							mizer.Wait()
-						} else {
-							alog.Fatalln(err, "error while executing atomizer")
-						}
-					} else {
-						alog.Fatalln(err, "atomizer instance returned nil")
-					}
-				} else {
-					alog.Fatalln(err, "conductor was returned nil")
-				}
-			} else {
-				alog.Fatalln(err, "error while initializing conductor")
-			}
-		} else {
-			alog.Fatalln(err, "error while pulling environment variables")
-		}
-
-		time.Sleep(time.Millisecond * 50)
-
-		// Get the alog wait method to work with the internal channels
-		alog.Wait(true)
-	} else {
-		alog.Fatalln(nil, "unable to overwrite the global logger")
+	if err != nil {
+		fmt.Println("unable to overwrite the global logger")
 	}
+
+	env := flag.Bool("e", false, "signals to the agent to use environment variables for configurations")
+	c := flag.String("conn", "amqp://guest:guest@localhost:5672/", "connection string used for rabbit mq")
+	q := flag.String("queue", "atomizer", "queue is the queue for atom messages to be passed across in the message queue")
+	flag.Parse()
+
+	if *env {
+		*c, *q, err = environment()
+	}
+
+	if err != nil {
+		fmt.Println("error while pulling environment variables | " + err.Error())
+		os.Exit(1)
+	}
+
+	// Create the amqp conductor for the agent
+	conductor, err := amqp.Connect(ctx, *c, *q)
+	if err != nil || conductor == nil {
+		fmt.Println("error while initializing amqp | " + err.Error())
+		os.Exit(1)
+	}
+
+	// Register the conductor into the atomizer library after initializing the
+	/// connection to the message queue
+	err = atomizer.Register(conductor)
+	if err != nil {
+		fmt.Println("error registering amqp conductor | " + err.Error())
+		os.Exit(1)
+	}
+
+	// setup the alog event subscription
+	events := make(chan interface{})
+	defer close(events)
+
+	alog.Printc(ctx, events)
+
+	// Create a copy of the atomizer
+	a := atomizer.Atomize(ctx, events)
+	if a == nil {
+		fmt.Println("atomizer instance returned nil")
+		os.Exit(1)
+	}
+
+	// Execute the processing on the atomizer
+	err = a.Exec()
+	if err != nil {
+		fmt.Println("error while executing atomizer | " + err.Error())
+		os.Exit(1)
+	}
+
+	alog.Println("Online")
+
+	// Block until the processing is interrupted
+	a.Wait()
+
+	time.Sleep(time.Millisecond * 50)
+
+	// Get the alog wait method to work with the internal channels
+	alog.Wait(true)
 }
 
-// envoverride pulls the environment variables as defined in the constants
+// environment pulls the environment variables as defined in the constants
 // section and overwrites the passed flag values
-func envoverride() (c, q string, err error) {
+func environment() (c, q string, err error) {
 
 	if c = os.Getenv(CONNECTIONSTRING); len(c) > 0 {
 		if q = os.Getenv(QUEUE); len(q) > 0 {
